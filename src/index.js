acbe343be4a6a754cab63c961d72d7c93c7f36f4
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-02 17:01:46 +0900

    feat(src/index.js memo.md): スプレッド構文について

--- src/index.js
+++ src/index.js
@@ -35,6 +35,7 @@
 
 // // constで定義した配列はプロパティの変更が可能
 
+// 分割代入
 // const myProfile = ["たえます", 27];
 // const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
 // console.log(message3);
@@ -44,5 +45,32 @@
 // console.log(message4);
 
 // デフォルト値、引数など
-const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
-sayHello();
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello();
+
+// スプレッド構文
+// 配列の展開
+// const arr1 = [1, 2, 3];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+console.log(arr6);
+
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
