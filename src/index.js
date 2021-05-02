6fafa901eaf9f14c884567c064e91898ce5e0510
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-02 16:29:40 +0900

    feat(src/index.js): 4-21 分割代入

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
-// import "./styles.css";
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // var変数は上書き可能
+// val1 = "var変数は上書き可能";
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書き可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // letは再宣言不可能
+// // let val2 = "let変数を再宣言";
+
+// const val3 = "constを宣言";
+// console.log(val3);
+
+// const val3 = "const変数を再宣言";
+
+// constで定義したプロパティはプロパティの変更が可能
+const val4 = {
+  name: "Takahiro",
+  age: 27,
+};
+val4.name = "taemasu";
+val4.address = "Japan";
+console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+
+const myProfile = ["たえます", 27];
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message4);
