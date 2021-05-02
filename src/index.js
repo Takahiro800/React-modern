83d6007c3f85fbc43e7303af2cad81ef6f66400e
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-02 16:36:33 +0900

    feat(src/index.js): デフォルト引数について

--- src/index.js
+++ src/index.js
@@ -25,20 +25,24 @@
 // const val3 = "const変数を再宣言";
 
 // constで定義したプロパティはプロパティの変更が可能
-const val4 = {
-  name: "Takahiro",
-  age: 27,
-};
-val4.name = "taemasu";
-val4.address = "Japan";
-console.log(val4);
+// const val4 = {
+//   name: "Takahiro",
+//   age: 27,
+// };
+// val4.name = "taemasu";
+// val4.address = "Japan";
+// console.log(val4);
 
-// constで定義した配列はプロパティの変更が可能
+// // constで定義した配列はプロパティの変更が可能
 
-const myProfile = ["たえます", 27];
-const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-console.log(message3);
+// const myProfile = ["たえます", 27];
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
 
-const [name, age] = myProfile;
-const message4 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message4);
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+// デフォルト値、引数など
+const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+sayHello();
