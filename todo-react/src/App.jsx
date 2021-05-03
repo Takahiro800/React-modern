3b2fd384047437f91d18bd9ccf34caa4715031cc
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-04 06:20:09 +0900

    feat(todo-react/app.jsx): 52 Reactでの実践を意識したモックに変更

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 
 export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState(["あああああ","いいいいいいい"]);
+  const [completeTodos, setCompleteTodos] = useState(["ううううう"])
+
   return (
     <>
       <div className="inputArea">
@@ -11,25 +14,28 @@ export const App = () => {
       <div className="incompleteArea">
         <p>未完了のTODO</p>
         <ul>
-          <li className="listItem">
-            <p className="listTitle">aaaa</p>
-            <button>完了</button>
-            <button>削除</button>
-          </li>
-          <li className="listItem">
-            <p className="listTitle">iiiiiiiiiiii</p>
-            <button>完了</button>
-            <button>削除</button>
-          </li>
+          {incompleteTodos.map((todo) => {
+            return (
+              <li key={todo} className="listItem">
+                <p className="listTitle">{todo}</p>
+                <button>完了</button>
+                <button>削除</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="completeArea">
         <p>完了済のTODO</p>
         <ul>
-          <li className="listItem">
-            <p className="listTitle">aaaa</p>
-            <button>戻す</button>
-          </li>
+          {completeTodos.map((todo) => {
+            return (
+              <li key={todo} className="listItem">
+                <p className="listTitle">{todo}</p>
+                <button>戻す</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
