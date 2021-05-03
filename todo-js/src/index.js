26eada6181f1ba6781a26cc5354d696c1fcf3829
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-03 09:39:28 +0900

    feat(todo-js/index.js): 33 タスクの追加機能

--- todo-js/src/index.js
+++ todo-js/src/index.js
@@ -12,8 +12,24 @@ const onClickAdd = () => {
   listTitle.className = "list__title";
   listTitle.innerText = inputText;
 
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
   // divタグの子要素に各要素を設定
   listItem.appendChild(listTitle);
+  listItem.appendChild(completeButton);
+  listItem.appendChild(deleteButton);
 
   // 未完了リストん追加
   document.getElementById("incomplete-lists").appendChild(listItem);
