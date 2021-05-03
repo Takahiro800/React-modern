c170520b929fcad0692eb2fa563094a5dd131a46
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-03 09:32:52 +0900

    feat(todo-js/index): 32 タスクの追加機能（テキスト）

--- todo-js/src/index.js
+++ todo-js/src/index.js
@@ -1,5 +1,22 @@
 const onClickAdd = () => {
-  alert();
+  // テキストボックスの値を取得して、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // li生成
+  const listItem = document.createElement("li");
+  // listItem.className = ""
+
+  // 子要素作成
+  const listTitle = document.createElement("div");
+  listTitle.className = "list__title";
+  listTitle.innerText = inputText;
+
+  // divタグの子要素に各要素を設定
+  listItem.appendChild(listTitle);
+
+  // 未完了リストん追加
+  document.getElementById("incomplete-lists").appendChild(listItem);
 };
 
 document
