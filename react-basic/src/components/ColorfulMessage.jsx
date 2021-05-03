1e09f9742a892680f3a7054a8be48b0fb1126d66
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-03 17:03:57 +0900

    feat(colorfulmessage.jsc): jsのプロパティ名を省略

--- react-basic/src/components/ColorfulMessage.jsx
+++ react-basic/src/components/ColorfulMessage.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const ColorfulMessage = (props) => {
+  const {color, children} = props;
   const contentStyle = {
-    color: props.color,
+    // jsはオブジェクトのプロパティ名と渡す名前が同じ場合は省略して記述できる
+    // color: color,
+    color,
     fontSize: '18px',
   };
 
   return (
-      <p style={contentStyle}>{ props.children }</p>
+      <p style={contentStyle}>{ children }</p>
   );
 }
 
