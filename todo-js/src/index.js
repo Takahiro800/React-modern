49a7e4147f98053fe25d9e689033cdf75fc6697c
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-03 10:38:15 +0900

    feat(todo-js/src/index.js): 36 タスクの戻す機能

--- todo-js/src/index.js
+++ todo-js/src/index.js
@@ -3,6 +3,16 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteList(inputText);
+};
+
+// 未完了のリストから指定の要素を削除
+const delteteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-lists").removeChild(target);
+};
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
   // li生成
   const listItem = document.createElement("li");
   // listItem.className = ""
@@ -10,7 +20,7 @@ const onClickAdd = () => {
   // 子要素作成
   const listTitle = document.createElement("div");
   listTitle.className = "list__title";
-  listTitle.innerText = inputText;
+  listTitle.innerText = text;
 
   // button(完了)タグ生成
   const completeButton = document.createElement("button");
@@ -36,6 +46,15 @@ const onClickAdd = () => {
     // buttonタグ生成
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // 押された戻すボタンの親タグを完了リストから削除
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-lists").removeChild(deleteTarget);
+
+      // テキストを取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
 
     addTarget.appendChild(listTitle);
     addTarget.appendChild(backButton);
@@ -60,11 +79,6 @@ const onClickAdd = () => {
 
   // 未完了リストの追加
   document.getElementById("incomplete-lists").appendChild(listItem);
-};
-
-// 未完了のリストから指定の要素を削除
-const delteteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-lists").removeChild(target);
 };
 
 document
